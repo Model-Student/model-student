1996ccf88700475ed00dacbb5c8f46b2f3e7c159
notebook <dongmin3147@gmail.com>
2022-06-24 11:21:33 +0900

    Splash 화면 구현/ '잘난체' 폰트 추가/ 이슈 없음

---
+++ app/src/main/java/com/tnedutsledom/modelstudent/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.tnedutsledom.modelstudent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent_view_change = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent_view_change);
+                finish();
+            }
+        },1500);
+    }
+}
