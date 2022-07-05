a0aceb345333c38c4a7f7a721804b661b1af30b3
notebook <dongmin3147@gmail.com>
2022-07-05 15:32:48 +0900

    UserSelect 기능 완성/ 이슈 없음

--- app/src/main/java/com/tnedutsledom/modelstudent/UserSelectActivity.java
+++ app/src/main/java/com/tnedutsledom/modelstudent/UserSelectActivity.java
@@ -1,14 +1,53 @@
 package com.tnedutsledom.modelstudent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserSelectActivity extends AppCompatActivity {
+    ImageView iv_user_select_child,iv_user_select_parent;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_select);
+
+        init();
+        onClick();
+    }
+    void init(){
+        iv_user_select_child= findViewById(R.id.iv_user_select_child);
+        iv_user_select_parent = findViewById(R.id.iv_user_select_parent);
+    }
+    void onClick(){
+        iv_user_select_child.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent_view_change = new Intent(getApplicationContext(),SignUpActivity.class);
+                intent_view_change.putExtra("User_select","child");
+                startActivity(intent_view_change);
+                intent_view_change.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+                return false;
+            }
+        });
+
+        iv_user_select_parent.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent_view_change = new Intent(getApplicationContext(),SignUpActivity.class);
+                intent_view_change.putExtra("User_select","parent");
+                startActivity(intent_view_change);
+                intent_view_change.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+                return false;
+            }
+        });
     }
 }
