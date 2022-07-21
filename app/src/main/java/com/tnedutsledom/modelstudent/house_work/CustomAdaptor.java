a59993c0ee13dd740486bcda234684246b24e567
roncan <65841016+roncan1@users.noreply.github.com>
2022-07-21 21:53:47 +0900

    체크박스 기능 구현 / 이슈 없음

--- app/src/main/java/com/tnedutsledom/modelstudent/house_work/CustomAdaptor.java
+++ app/src/main/java/com/tnedutsledom/modelstudent/house_work/CustomAdaptor.java
@@ -2,16 +2,22 @@ package com.tnedutsledom.modelstudent.house_work;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -28,11 +34,14 @@ public class CustomAdaptor extends BaseAdapter {
     LayoutInflater layoutInflater = null;
     public ArrayList<Work> workArrayList;
     public View house_work_item;
+    Animation anim_alpha_100, anim_alpha_0;
 
     public CustomAdaptor(Context context, ArrayList<Work> data) {
         this.context = context;
         this.workArrayList = data;
         layoutInflater = LayoutInflater.from(context);
+        anim_alpha_100 = AnimationUtils.loadAnimation(context, R.anim.alpha100);
+        anim_alpha_0 = AnimationUtils.loadAnimation(context, R.anim.alpha0);
     }
 
     @Override
@@ -53,8 +62,24 @@ public class CustomAdaptor extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         house_work_item = layoutInflater.inflate(R.layout.house_work_item, null);
-        TextView tv_work_name = house_work_item.findViewById(R.id.tv_hw_work_name);
         ImageView iv_category = house_work_item.findViewById(R.id.iv_hw_category);
+        TextView tv_work_name = house_work_item.findViewById(R.id.tv_hw_work_name);
+        ImageView iv_selected = house_work_item.findViewById(R.id.iv_hw_selected);
+
+        iv_selected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (workArrayList.get(i).getSelected()) {
+                    iv_selected.startAnimation(anim_alpha_0);
+                    workArrayList.get(i).setSelected(false);
+                    Log.d("sdsadda", "onClick: " + workArrayList.get(i).getSelected());
+                } else if (!workArrayList.get(i).getSelected()) {
+                    iv_selected.startAnimation(anim_alpha_100);
+                    workArrayList.get(i).setSelected(true);
+                    Log.d("sdsadda", "onClick: " + workArrayList.get(i).getSelected());
+                }
+            }
+        });
 
         tv_work_name.setText(workArrayList.get(i).getWork_name());
         switch (workArrayList.get(i).getCategory()){
@@ -67,4 +92,5 @@ public class CustomAdaptor extends BaseAdapter {
 
         return house_work_item;
     }
+
 }
--- app/src/main/java/com/tnedutsledom/modelstudent/house_work/Work.java
+++ app/src/main/java/com/tnedutsledom/modelstudent/house_work/Work.java
@@ -3,10 +3,18 @@ package com.tnedutsledom.modelstudent.house_work;
 public class Work {
     String work_name;
     String category;
+    boolean selected = true;
 
     public Work(String work_name, String category) {
         this.work_name = work_name;
         this.category = category;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+    public boolean getSelected() {
+        return selected;
     }
 
     public String getCategory() {
