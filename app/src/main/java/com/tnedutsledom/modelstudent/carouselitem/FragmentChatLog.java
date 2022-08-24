4ef69541715c2e8148b689b48b511c541308a9e1
roncan1 <carugame12_@naver.com>
2022-08-24 11:28:01 +0900

    메인 액티비티의 아이의 하루 부분을 자녀 이름으로 표시되도록 구현 / 임시로 유저 데이터 삭제 기능 구현 / 이슈 없음

--- app/src/main/java/com/tnedutsledom/modelstudent/carouselitem/FragmentChatLog.java
+++ app/src/main/java/com/tnedutsledom/modelstudent/carouselitem/FragmentChatLog.java
@@ -1,11 +1,15 @@
 package com.tnedutsledom.modelstudent.carouselitem;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -17,6 +21,7 @@ import com.tnedutsledom.modelstudent.house_work.HouseWorkActivity;
 public class FragmentChatLog extends Fragment {
 
     LinearLayout ll_chat_log;
+    TextView tv_chat_log;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,6 +37,23 @@ public class FragmentChatLog extends Fragment {
                 getActivity().overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
             }
         });
+
+        tv_chat_log = rootView.findViewById(R.id.tv_chat_log);
+        setTv_chat_log(tv_chat_log);
         return rootView;
+    }
+
+    String getChildName() {
+        SharedPreferences preferences = getActivity().getSharedPreferences("user_info",MODE_PRIVATE);
+        String childName = preferences.getString("child_name", "");
+        if (childName.length() < 3) {
+            return childName;
+        } else {
+            return childName.substring(1);
+        }
+    }
+
+    void setTv_chat_log(TextView tv) {
+        tv.setText(getChildName() + "의\n하루");
     }
 }
